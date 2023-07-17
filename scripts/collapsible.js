22b99f27d09fb70714eb38e1940e72897d7bf9a1
mpEmperor <multaniprabnoor71@gmail.com>
2023-07-16 23:14:06 -0400

    new commit

--- scripts/collapsible.js
+++ scripts/collapsible.js
@@ -2,7 +2,6 @@ const coll = document.getElementsByClassName("collapsible-btn");
 
 for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    console.log("click");
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
