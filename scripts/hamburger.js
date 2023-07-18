5632b426bbfbd3362060770dabd53c1bbf1c1e01
mpEmperor <multaniprabnoor71@gmail.com>
2023-07-17 21:43:09 -0400

    fixed scrolling on mobile nav

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -3,11 +3,11 @@ const overlay = document.getElementById("overlay");
 
 hamburgerBtn.onclick = function() {
   if (overlay.style.height != "100%") {
+    document.body.style.overflow = "hidden";
     overlay.style.height = "100%";
-    document.body.style.overflowY = "hidden";
     hamburgerBtn.innerHTML = "<i class='fa-solid fa-x fa-xl' style='color: #000000;'></i>"
   } else {
-    document.body.style.overflowY = "initial";
+    document.body.style.overflow = "initial";
     overlay.style.height = "0%";
     hamburgerBtn.innerHTML = "<i class='fa-solid fa-bars fa-xl' style='color: #000000;'></i>"
   }
@@ -17,7 +17,7 @@ const screenSize = window.matchMedia("(max-width: 800px)");
 screenSize.addEventListener("change", () => {
     if (!screenSize.matches) {
       overlay.style.height = "0%";
-      document.body.style.overflowY = "initial";
+      document.body.style.overflow = "initial";
       hamburgerBtn.innerHTML = "<i class='fa-solid fa-bars fa-xl' style='color: #000000;'></i>"
     }
   }
