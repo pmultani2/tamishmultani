5a07bc524d3c4302025a24d048896098f7e7c47c
mpEmperor <multaniprabnoor71@gmail.com>
2023-07-18 21:00:10 -0400

    improved mobile nav

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,14 +1,19 @@
 const hamburgerBtn = document.getElementById("hamburger");
 const overlay = document.getElementById("overlay");
+const overlayNav = overlay.firstElementChild;
 
 hamburgerBtn.onclick = function() {
-  if (overlay.style.height != "100%") {
+  if (overlay.style.opacity != "1") {
     document.body.style.overflow = "hidden";
-    overlay.style.height = "100%";
+    overlay.style.visibility = "visible";
+    overlay.style.opacity = "1";
+    overlayNav.style.transform = "translate(0, -15px)";
     hamburgerBtn.innerHTML = "<i class='fa-solid fa-x fa-xl' style='color: #000000;'></i>"
   } else {
     document.body.style.overflow = "initial";
-    overlay.style.height = "0%";
+    overlay.style.visibility = "hidden";
+    overlayNav.style.transform = "translate(0, 15px)";
+    overlay.style.opacity = "0";
     hamburgerBtn.innerHTML = "<i class='fa-solid fa-bars fa-xl' style='color: #000000;'></i>"
   }
 };
@@ -16,7 +21,9 @@ hamburgerBtn.onclick = function() {
 const screenSize = window.matchMedia("(max-width: 800px)");
 screenSize.addEventListener("change", () => {
     if (!screenSize.matches) {
-      overlay.style.height = "0%";
+      overlayNav.style.transform = "translate(0, 15px)";
+      overlay.style.visibility = "hidden";
+      overlay.style.opacity = "0";
       document.body.style.overflow = "initial";
       hamburgerBtn.innerHTML = "<i class='fa-solid fa-bars fa-xl' style='color: #000000;'></i>"
     }
